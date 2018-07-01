9c8d7482c81a712e6de114138d3e5cb60fcd893b
mikezarathustra <mikemunoz@yahoo.com>
2018-07-01 11:54:47 -0500

    mi primera version

---
+++ app/src/main/java/com/signoperu/mynewapp/Main4Activity.java
@@ -0,0 +1,63 @@
+package com.signoperu.mynewapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class Main4Activity extends AppCompatActivity {
+
+    Button miboton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        miboton=findViewById(R.id.button4);
+        miboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrir1= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(abrir1);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opcion1:
+                Intent v1 = new Intent(this, MainActivity.class);
+                startActivity(v1);
+                return true;
+
+            case R.id.opcion2:
+                Intent v2 = new Intent(this, MainActivity.class);
+                startActivity(v2);
+                return true;
+
+            case R.id.opcion3:
+                Intent v3 = new Intent(this, MainActivity.class);
+                startActivity(v3);
+                return true;
+
+            case R.id.opcion4:
+                Intent v4 = new Intent(this, MainActivity.class);
+                startActivity(v4);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.menu_uno,m);
+        return true;
+
+    }
+}
---
+++ app/src/main/java/com/signoperu/mynewapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.signoperu.mynewapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button miboton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        miboton=findViewById(R.id.button1);
+        miboton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrir2 = new Intent(getApplicationContext(),Main2Activity.class);
+                startActivity(abrir2);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opcion1:
+                Intent v1 = new Intent(this, MainActivity.class);
+                startActivity(v1);
+                return true;
+
+            case R.id.opcion2:
+                Intent v2 = new Intent(this, MainActivity.class);
+                startActivity(v2);
+                return true;
+
+            case R.id.opcion3:
+                Intent v3 = new Intent(this, MainActivity.class);
+                startActivity(v3);
+                return true;
+
+            case R.id.opcion4:
+                Intent v4 = new Intent(this, MainActivity.class);
+                startActivity(v4);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.menu_uno,m);
+        return true;
+
+    }
+}
